ea35a9ad8bd287e154edd432c526bc15c4fe5373
Hitender <ktra210@gmail.com>
2023-12-07 12:50:53 +0530

    Add files via upload
    Multiple of 5 or not JS code

---
+++ script.js
@@ -0,0 +1,7 @@
+let issNumber = prompt("Enter Your Number");
+let issMultiple = issNumber % 5;
+if ( issMultiple = 1) {
+    console.log("this is multiple of 5 = ", issNumber);
+} else {
+    console.log("Not a multiple of 5 = ", issNumber);
+}
